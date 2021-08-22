b80c1c9e888fd0dbc8150813951bf3a80c2f5e00
WiwiW21 <wilyrustam@gmail.com>
2021-08-22 12:34:41 +0700

    new

--- try.R
+++ try.R
@@ -1,7 +1,16 @@
 #mengcoba ya
-teks1 = "Hello World"
-teks2 = "mengcoba"
+# Digunakan untuk mengecek apakah paket yang dibutuhkan sudah
+# terinstall atau belum. Jika belum install.package()
+install_paket <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+# usage
+paket <- c("tidyverse", "tidytext", "reshape2", "RColorBrewer", "scales",
+           "haven","topicmodels", "plotly", "DT", "knitr", "readxl", "missForest")
+install_paket(paket)
+library
 
-teks1
-teks2
-
+sebuah_data
